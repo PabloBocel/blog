a25034f707f35a61b07db427f14cab2124d76b8c
PabloBocel <123261458+PabloBocel@users.noreply.github.com>
2026-03-17 21:15:16 -0600

    modulo 7

--- resources/views/Posts/index.blade.php
+++ resources/views/Posts/index.blade.php
@@ -5,35 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Coders Free</title>
-
-    <style>
-        .colo-red {
-            color: red;
-        }
-        .color-green {
-            color: green;
-        }
-    </style>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
     <h1>Listado de Posts</h1>
 
 
-    <form action="">
-        <div>
-            <label>
-                <input type="checkbox" name="check1" id="check1">
-                Perú
-            </label>
-            <label>
-                <input type="checkbox" name="check2" id="check2">
-                Chile
-            </label>
-            <label>
-                <input type="checkbox" name="check3" id="check3">
-                Argentina
-            </label>
-        </div>
-    </form>
+    <div class="container mx-auto py-12">
+        <x-alert>
+            Hola mundo
+        </x-alert>
+    </div>
 </body>
 </html>
